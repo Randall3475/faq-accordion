247b1b92b139bf53fae97ed7f22cff217ddb1f1b
Randall <randall3475@proton.me>
2023-12-15 17:19:16 +0100

    Update Accordion.tsx

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -2,27 +2,28 @@ import { useState } from 'react'
 import { IconPlus, IconMinus } from './ui'
 
 type AccordionProps = {
+  key: string | number
   question: string
   answer: string
 }
 
-export default function Accordion({ question, answer }: AccordionProps) {
+export default function Accordion({ key, question, answer }: AccordionProps) {
   const [accordionOpen, setAccordionOpen] = useState<boolean>(false)
   return (
     <div>
-      <summary
+      <button
         aria-expanded={accordionOpen}
-        aria-controls='accordion-content'
+        aria-controls={`accordion-content-${key}`}
         onClick={() => setAccordionOpen(!accordionOpen)}
-        className='group flex cursor-pointer items-center justify-between'
+        className='group flex w-full cursor-pointer items-center justify-between gap-x-4 text-start'
       >
         <h2 className='text-lg font-semibold text-zinc-800 transition-colors group-hover:text-[#AD28EB]'>
           {question}
         </h2>
         {accordionOpen ? <IconMinus /> : <IconPlus />}
-      </summary>
+      </button>
       <p
-        id='accordion-content'
+        id={`accordion-content-${key}`}
         className={`text-sm font-normal leading-normal text-zinc-500 transition-all lg:text-base ${
           accordionOpen ? 'mt-6' : 'mt-0 h-0 overflow-hidden'
         }`}
